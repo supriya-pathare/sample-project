e6db28824afd10a0071c814252bfdf64e0dfa994
Supriya <supriya.pathare@gslab.com>
2021-04-04 12:51:43 +0530

    Python File

---
+++ list_supriya.py
@@ -0,0 +1,9 @@
+thislist = ["apple", "banana", "cherry"]
+print(thislist)
+
+mylist = ["apple", "banana", "cherry"]
+print(type(mylist))
+
+thislist = ["apple", "banana", "cherry"]
+thislist.insert(1, "orange")
+print(thislist)
