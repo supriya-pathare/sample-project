c70426946bc6fcd1c057daf49b3cc1acfaa76543
supriya-pathare <81814435+supriya-pathare@users.noreply.github.com>
2021-04-05 13:02:28 +0530

    Update list_supriya.py

--- list_supriya.py
+++ list_supriya.py
@@ -5,6 +5,6 @@ print(thislist)
 print(type(thislist))
 print(thislist[-2])
 thislist = [10,20,30,40,50]
-thislist.append(60)
+thislist.append(70)
 print(thislist)
 print('this is updated code to check automatic build trigger')
