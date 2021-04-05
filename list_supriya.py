8e0533393b25b94acf70a0a8fe66ea3712f08d5a
supriya-pathare <81814435+supriya-pathare@users.noreply.github.com>
2021-04-05 13:00:44 +0530

    Update list_supriya.py

--- list_supriya.py
+++ list_supriya.py
@@ -1,10 +1,10 @@
+print("Simple demo application")
 thislist = ["apple", "banana", "cherry"]
 print(thislist)
 
-mylist = ["apple", "banana", "cherry"]
 print(type(mylist))
-
-thislist = ["apple", "banana", "cherry"]
-thislist.insert(1, "orange")
+print(thislist[-2])
+thislist = [10,20,30,40,50]
+thislist.append(60)
 print(thislist)
 print('this is updated code to check automatic build trigger')
